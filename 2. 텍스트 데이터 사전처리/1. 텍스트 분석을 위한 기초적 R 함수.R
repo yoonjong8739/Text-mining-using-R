58ee434f267ab99215fb390e734df2925d449f64
yoonjong8739 <namyoonjong@gmail.com>
2025-07-08 00:00:53 +0900

    코드 오류 수정

--- 2. 텍스트 데이터 사전처리/1. 텍스트 분석을 위한 기초적 R 함수.R
+++ 2. 텍스트 데이터 사전처리/1. 텍스트 분석을 위한 기초적 R 함수.R
@@ -1,9 +1,9 @@
 ## list(): 리스트 형식의 오브젝트
 # 리스트와 벡터의 구분
-myvector <- c(1:6, 'a') # 단 하나의 속성으로만 표현 가능(원자적 특성).
+myvector <- c(1:6, "a") # 단 하나의 속성으로만 표현 가능(원자적 특성).
 myvector
 
-mylist <- list(1:6, 'a') # 여러 형식의 데이터 포함
+mylist <- list(1:6, "a") # 여러 형식의 데이터 포함
 mylist
 
 # list 형식의 오브젝트 소개
@@ -23,8 +23,8 @@ mylist[[3]][[1]]
 mylist[[3]][[1]][2]
 
 # unlist 함수는 리스트를 벡터 형식으로 돌려주는 역할을 한다.
-myvector <- c(1:6, 'a')
-mylist <- list(1:6, 'a')
+myvector <- c(1:6, "a")
+mylist <- list(1:6, "a")
 unlist(mylist)
 unlist(mylist) == myvector
 
@@ -39,15 +39,15 @@ list(name1, myspace, name2)
 unlist(list(name1, myspace, name2))
 
 ## attr(): 오브젝트에 속성값을 입력하거나 추출하기
-name <- c('갑', '을', '병', '정')
-gender <- c(2,1,1,2)
+name <- c("갑", "을", "병", "정")
+gender <- c(2, 1, 1, 2)
 mydata <- data.frame(name, gender)
 attr(mydata$name, "what the variable means") <- "응답자의 이름"
 mydata$name
 
 attr(mydata$gender, "what the variable means") <- "응답자의 성별"
 myvalues <- gender
-for (i in 1:length(gender)) {
+for (i in seq_along(gender)) {
   myvalues[i] <- ifelse(gender[i] == 1, "남성", "여성")
 }
 attr(mydata$gender, "what the value means") <- myvalues
@@ -66,16 +66,16 @@ mylist
 
 lapply(mylist[[3]], mean)
 lapply(mylist, mean)
-lapply(mylist[c(1, 2, c(1,2))], mean) # 어떤 형식으로 구성되어 있는지 밝히면 됨.
+lapply(mylist[c(1, 2, c(1, 2))], mean) # 어떤 형식으로 구성되어 있는지 밝히면 됨.
 
 # sapply 함수의 경우 lapply 함수 결과와 유사하지만 결괏값에 unlist 함수를 적용한 것과 동일.
-sapply(mylist[c(1,2, c(1,2))], sum)
-unlist(lapply(mylist[c(1, 2, c(1,2))], sum))
+sapply(mylist[c(1, 2, c(1, 2))], sum)
+unlist(lapply(mylist[c(1, 2, c(1, 2))], sum))
 
 # tapply는 텍스트 데이터에서 종종 사용됨.
 wordlist <- c("the", "is", "a", "the")
-doc1freq <- c(3,4,2,4)
-doc2freq <- rep(1,4)
+doc1freq <- c(3, 4, 2, 4)
+doc2freq <- rep(1, 4)
 tapply(doc1freq, wordlist, length)
 tapply(doc2freq, wordlist, length)
 tapply(doc1freq, wordlist, sum)
